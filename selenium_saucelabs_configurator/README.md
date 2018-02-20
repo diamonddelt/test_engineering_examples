b32d6ba97e7ec1f4e778c303d8e4eadbab07c53d
diamonddelt <rrrasti@yahoo.com>
2018-02-19 19:23:55 -0600

    Modify folder names and READMEs

---
+++ selenium_saucelabs_configurator/README.md
@@ -0,0 +1,14 @@
+# Author: Ryan Rasti
+
+# System Requirements
+1. Install node.js, npm, protractorjs, webdriver-manager, and ensure they are all accessible on the system PATH
+2. (optional) Install a good terminal program to multiplex two terminal windows
+
+# Running the scenario
+1. cd into the 'selenium_saucelabs_configurator' directory
+2. $ webdriver-manager start
+3. Open a new terminal window in the same directory as above
+4. $ protractorjs conf.js
+
+# Notes
+1. This example intends to test how one can automate AngularJS UIs using Selenium. Since I am not a strong Javascript developer, and have little experience with both AngularJS and ProtractorJS, I expect a lot of room for improvement in this example. Just trying to do something rather simple like this with both a language I am weak with, as well as a test framework I have never used, proves to be quite challenging to me.
--- selenium_saucelabs_configurator/spec.js
+++ selenium_saucelabs_configurator/spec.js
@@ -27,7 +27,7 @@ describe('SauceLabs Platform Configurator Test', function() {
     it('should open the "Advanced Configuration" menu', function() {
         element(by.xpath('//h4[contains(text(), "Show Advanced Configuration")]')).click();
         // expected condition here
-        browser.pause();
+        // browser.pause();
     });
 
     it('should toggle options in the "Advanced Configuration" menu', function() {
@@ -40,15 +40,34 @@ describe('SauceLabs Platform Configurator Test', function() {
 
     it('should verify the java "COPY CODE" is expected output', function() {
         let codeBlock = element(by.xpath('//code[@class="hljs java"]'));
-        let desired_capabilities_string = 'DesiredCapabilities caps = DesiredCapabilities.internetExplorer();'
+        let desiredCapabilitiesString = 'DesiredCapabilities caps = DesiredCapabilities.internetExplorer();';
 
+        expect(codeBlock.getText()).toContain(desiredCapabilitiesString); // verifies the first string is present
+        // expect(innerSpans.length == 8); // verify if all of the other configurations are present
 
-        expect(codeBlock.getText()).toContain(desired_capabilities_string); // this seems to work
+        // verify the inner spans are correct before messy string construction
+        let innerSpans = [];
+        codeBlock.all(by.tagName('span')).then(function(items){
+            expect(items.length).toBe(8); // verify the number of configurations are present
 
-        // how do I do this? lol
-        browser.pause();
+            // verify the configurations themselves are present
+            expect(items[0].getText()).toBe('"platform"');
+            expect(items[1].getText()).toBe('"Windows 10"');
+            expect(items[2].getText()).toBe('"version"');
+            expect(items[3].getText()).toBe('"11.103"');
+            expect(items[4].getText()).toBe('"recordVideo"');
+            expect(items[5].getText()).toBe('"false"');
+            expect(items[6].getText()).toBe('"screenResolution"');
+            expect(items[7].getText()).toBe('"1280x800"');
+            
+            // innerSpans = items.slice(0);
+        });
+
+        // begin messy string construction
+        // Not sure of the best way to do this currently...
+        // let platformCapabilitiesString = 'caps.setCapability("' + innerSpans[0] + '\", \"' + innerSpans[1] + '\");';
+        
+        // MAKE LOOK LIKE THIS: caps.setCapability("platform", "Windows 10");
     });
-      // expect(element(by.binding('latest')).getText()).
-      //    toEqual('5'); // This is wrong!
   });
   
